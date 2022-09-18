78d4475b4af3e9488cddd1e533f5696f8254d6bb
ThulasiVasanth16 <108793655+ThulasiVasanth16@users.noreply.github.com>
2022-09-18 17:27:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Nee kavithaigala..',
+        path: 'assets/musics/song1.mp3',
+        artist: 'Pradeep Kumar',
+        cover: 'assets/images/cover5.png'
+    },
+    {
+        name: 'Ennavale-Adi-Ennavale',
+        path: 'assets/musics/song2.mp3',
+        artist: 'A.R.Rahman',
+        cover: 'assets/images/cover2.png'
+    },
+    {
+        name: 'Yelomia',
+        path: 'assets/musics/song3.mp3',
+        artist: 'Andrea',
+        cover: 'assets/images/cover6.png'
+    },
+    {
+        name: 'En Kadhale',
+        path: 'assets/musics/song4.mp3',
+        artist: 'S.P.Balasubrahmanyam',
+        cover: 'assets/images/cover1.png'
+    },
+    {
+        name: 'Pogathye Pogathe',
+        path: 'assets/musics/song5.mp3',
+        artist: 'Yuvan Shankar Raja',
+        cover: 'assets/images/cover3.png'
+    },
+    {
+        name: 'Nee Korinaal',
+        path: 'assets/musics/song6.mp3',
+        artist: 'Shwetha Mohan',
+        cover: 'assets/images/cover8.png'
+    },
+    {
+        name: 'Theemai Dhaan Vellum',
+        path: 'assets/musics/song7.mp3',
+        artist: 'Hiphop Tamizha',
+        cover: 'assets/images/cover11.png'
+    },
+    {
+        name: 'Then Then Then',
+        path: 'assets/musics/song8.mp3',
+        artist: 'Shreya Ghoshal',
+        cover: 'assets/images/cover9.png'
+    },
+]
